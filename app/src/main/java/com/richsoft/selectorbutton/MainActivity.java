bb50b198f545ec12136188d54a16e534d3748633
chengjie <1007821660@qq.com>
2017-04-01 15:47:26 +0800

    上传截图

--- app/src/main/java/com/richsoft/selectorbutton/MainActivity.java
+++ app/src/main/java/com/richsoft/selectorbutton/MainActivity.java
@@ -1,13 +1,41 @@
 package com.richsoft.selectorbutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.richsoft.selectorlibrary.SelectorButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SelectorButton mSelectorButtonRed;
+    private SelectorButton mSelectorButtonTeal;
+    private SelectorButton mSelectorButtonBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSelectorButtonRed = (SelectorButton) findViewById(R.id.btn_red);
+        mSelectorButtonTeal = (SelectorButton) findViewById(R.id.btn_teal);
+        mSelectorButtonBlue = (SelectorButton) findViewById(R.id.btn_blue);
+
+        //设置mSelectorButtonTeal按钮不可点击
+        mSelectorButtonTeal.setEnabled(false);
+
+        mSelectorButtonRed.setOnClickListener(this);
+        mSelectorButtonBlue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_red:
+                Toast.makeText(this, "点击了", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_blue:
+                Toast.makeText(this, "点击了", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
